a08d405d2ffcd45bb110dab420609c4f16022c83
manyyeon <udayeon@naver.com>
2022-06-04 20:58:54 +0900

    홈으로 가는 기능 만들기

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ cursor = db.cursor(pymysql.cursors.DictCursor)
 cursor.execute('USE s2020112547_final_project;')
 
 options = ["회원", "메뉴", "매장", "회원방문관리", "본사정보", "재료재고정보", "재료공급내역", "재료사용기록", "시스템종료"]
-subOptions = [["전체회원조회", "아이디로회원검색", "회원등록"], ["전체메뉴조회", "메뉴검색", "메뉴추가"], ["전체매장조회", "매장검색", "매장추가"], ["전체방문기록조회", "매장별 방문기록조회", "방문기록추가"], ["본사정보조회", "본사정보수정"], ["전체재고조회", "재고검색", "재료추가"], ["전체공급내역조회", "공급내역검색", "공급내역등록"], ["전체재료사용내역조회", "사용내역검색", "사용내역등록"]]
+subOptions = [["전체회원조회", "아이디로회원검색", "회원등록", "홈으로"], ["전체메뉴조회", "메뉴검색", "메뉴추가", "홈으로"], ["전체매장조회", "매장검색", "매장추가", "홈으로"], ["전체방문기록조회", "매장별 방문기록조회", "방문기록추가", "홈으로"], ["본사정보조회", "본사정보수정", "홈으로"], ["전체재고조회", "재고검색", "재료추가", "홈으로"], ["전체공급내역조회", "공급내역검색", "공급내역등록", "홈으로"], ["전체재료사용내역조회", "사용내역검색", "사용내역등록", "홈으로"]]
 bar = "--------------------------------------"
 
 # 기능 출력
@@ -36,12 +36,6 @@ def getSubOptionFromUser(select):
     print()
     return subSelect;
 
-def printMemberInfo(memberInfo):
-    print(bar)
-    for key in memberInfo:
-        print(key, "\t:", memberInfo.get(key))
-    print(bar)
-
 # 옵션 선택했을 때 상단바 출력해줌
 def printTopBar(optionName):
     stars = 18 - len(optionName)
@@ -55,6 +49,62 @@ def printTopBar(optionName):
         print("*", end="")
         i += 1
     print()
+
+# 데이터의 모든 정보 출력해줌
+def printAllInfoInData(userInfo):
+    print(bar)
+    for key in userInfo:
+        print(key, "\t:", userInfo.get(key))
+    print(bar)
+
+# 1. 1) 전체회원조회
+def viewAllUsers():
+    cursor.execute('SELECT * FROM 회원;')
+    userInfoList = cursor.fetchall()
+    i = 0
+    while(i<len(userInfoList)):
+        printAllInfoInData(userInfoList[i])
+        i += 1
+    db.commit()
+
+# 1. 2) 아이디로회원검색
+def searchUserById():
+    userIdInput = input('검색할 회원 아이디 입력 >> ')
+    cursor.execute('SELECT * FROM 회원 WHERE 회원아이디 = %s', userIdInput)
+            
+    userInfo = cursor.fetchall()
+    printAllInfoInData(userInfo[0])
+    print()
+    db.commit()
+
+# 1. 3) 회원등록
+def addNewUser():
+    userInfoInput = []
+    userInfoInput.append(input('아이디(중복안됨) >> '))
+    userInfoInput.append(input('이름 >> '))
+    userInfoInput.append(input('전화번호 >> '))
+
+    userInfoInput = tuple(userInfoInput)
+
+    cursor.execute('INSERT INTO 회원 VALUES(%s, %s, %s)', userInfoInput)
+    cursor.execute('SELECT * FROM 회원 WHERE 회원아이디 = %s', userInfoInput[0])
+
+    userInfo = cursor.fetchall()
+    print("\n회원이 등록되었습니다.")
+    printAllInfoInData(userInfo[0])
+    print()
+    db.commit()
+
+# 2. 1) 전체메뉴조회
+category = {1: "COFFEE", 2: "FRAPPUCCINO", 3: "TEA", 4: "CAKE"}
+def viewAllMenus():
+    cursor.execute('SELECT * FROM 메뉴;')
+    menuInfoList = cursor.fetchall()
+    i = 0
+    while(i<len(menuInfoList)):
+        printAllInfoInData(menuInfoList[i])
+        i += 1
+    db.commit()
 
 """
 --- 전체 기능 ---
@@ -92,86 +142,44 @@ def printTopBar(optionName):
 9. 시스템종료
 """
 
-print("-------- 스타벅스에 운영 시스템 --------")
+print("------- 스타벅스에 운영 시스템 -------")
 while(True):
     # 기능 계속 출력(종료 선택할 때까지)
     select = getOptionFromUser()
     printTopBar(options[select-1])
-    subSelect = getSubOptionFromUser(select)
-    printTopBar(subOptions[select-1][subSelect-1])
-
+        
+    # 1. 회원
     if(select == 1):
-        # 전체회원조회
-        if(subSelect == 1):
-            cursor.execute('SELECT * FROM 회원;')
-            memberInfoList = cursor.fetchall()
-            i = 0
-            while(i<len(memberInfoList)):
-                printMemberInfo(memberInfoList[i])
-                i += 1
-            db.commit()
-        elif(subSelect == 2):
-            userIdInput = input('검색할 회원 아이디 입력 >> ')
-            cursor.execute('SELECT * FROM 회원 WHERE 회원아이디 = %s', userIdInput)
-            
-            memberInfo = cursor.fetchall()
-            printMemberInfo(memberInfo[0])
-            print()
-            db.commit()
-
-            print(bar)
-            print("1. 회원 정보 수정\n2. 회원 삭제")
-            print(bar)
-            subSubSelect = int(input('더 세부적인 기능을 선택하세요 >> '))
-            if(subSubSelect == 1):
-                print("수정 기능 안됨")
-                memberInfoInput = []
-                userId = input('아이디(중복안됨) >> ')
-                memberInfoInput.append(userId)
-                memberInfoInput.append(input('이름 >> '))
-                memberInfoInput.append(input('전화번호 >> '))
-                memberInfoInput.append(userId) # WHERE = 회원아이디=%s를 위해서 아이디을 한 번 더 넣어줌
-
-                memberInfoInput = tuple(memberInfoInput)
-                print(memberInfoInput)
-                print(userId)
-                cursor.execute('UPDATE 회원 SET 회원아이디=%s, 회원이름=%s, 전화번호=%s WHERE 회원아이디=%s', memberInfoInput)
-                db.commit()
-                cursor.execute('SELECT * FROM 회원 WHERE 회원아이디 = %s', memberInfoInput[0])
-
-                memberInfo = cursor.fetchall()
-                print(memberInfo)
-                print("\n회원 정보가 수정되었습니다.")
-                printMemberInfo(memberInfo)
-                print()
-                db.commit()
-
-            elif(subSubSelect == 2):
-                print("\n************** 회원 삭제 **************")
-        # 회원등록
-        elif(subSelect == 3):
-            memberInfoInput = []
-            memberInfoInput.append(input('아이디(중복안됨) >> '))
-            memberInfoInput.append(input('이름 >> '))
-            memberInfoInput.append(input('전화번호 >> '))
-
-            memberInfoInput = tuple(memberInfoInput)
-
-            cursor.execute('INSERT INTO 회원 VALUES(%s, %s, %s)', memberInfoInput)
-            cursor.execute('SELECT * FROM 회원 WHERE 회원아이디 = %s', memberInfoInput[0])
-
-            memberInfo = cursor.fetchall()
-            print("\n회원이 등록되었습니다.")
-            printMemberInfo(memberInfo[0])
-            print()
-            db.commit()
+         while(True):
+            subSelect = getSubOptionFromUser(select)
+            printTopBar(subOptions[select-1][subSelect-1])
+            # 1) 전체회원조회
+            if(subSelect == 1):
+                viewAllUsers()
+            # 2) 아이디로회원검색
+            elif(subSelect == 2):
+                searchUserById()
+            # 3) 회원등록
+            elif(subSelect == 3):
+                addNewUser()
+            else:
+                print("홈으로 가기")
+                break
     elif(select == 2):
-        print("\n**************** 메뉴 ****************")
-        print("--------------------------------------")
-        print("1. 전체 회원 조회\n2. 아이디로 회원 검색\n3. 회원 등록")
-        print("--------------------------------------")
-        subSelect = int(input('세부 기능을 선택하세요 >> '))
+        while(True):
+            subSelect = getSubOptionFromUser(select)
+            printTopBar(subOptions[select-1][subSelect-1])
+            # 1) 전체메뉴조회
+            if(subSelect == 1):
+                viewAllMenus()
+            # 2) 메뉴검색
+            # 3) 메뉴추가
     elif(select == 3):
+        # 1) 전체매장조회
+        # if(subSelect == 1):
+
+        # 2) 매장검색
+        # 3) 매장추가
         print("\n**************** 매장 ****************")
     elif(select == 4):
         print("\n************ 회원 방문 관리 ************")
